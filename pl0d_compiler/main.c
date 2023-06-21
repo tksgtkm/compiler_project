22489f4d6b834baf2db5adcc2388b0152e8fdc24
tksgtkm <h9cd342fd7jgx36@gmail.com>
2023-06-21 09:00:35 +0900

    コメント追加

--- pl0d_compiler/main.c
+++ pl0d_compiler/main.c
@@ -3,12 +3,18 @@
 
 
 int main() {
+  // ソースプログラムファイルの名前
   char fileName[30];
   printf("enter source file name\n");
   scanf("%s", fileName);
+  // ソースプログラムファイルのopen
   if (!openSource(fileName))
+    // open失敗すれば終わり
     return;
+  // コンパイルして
   if (compile())
+    // エラーがなければ実行
     execute();
+  // ソースプログラムファイルのclose
   closeSource();
 }
