b38bd65f3b8facaf0db123f1f25f5f1ef2f84f2d
tksgtkm <h9cd342fd7jgx36@gmail.com>
2023-07-09 23:03:59 +0900

    トークン実装の続き

--- cci_compiler/cci_prot.h
+++ cci_compiler/cci_prot.h
@@ -1,5 +1,14 @@
 
-
+/* cci_tkn.c */
 void initChTyp();
 void fileOpen();
 // Token nextTkn();
+
+/* cci_misc.c */
+int is_kanji(int ch);
+int get_kanji_mode();
+char *s_malloc(char *s);
+void incVar(int *var, int size, char *errmsg);
+void err_s(char *s);
+void err_fi(char *fmt, int idt);
+void err_ss(char *s1, char *s2);
